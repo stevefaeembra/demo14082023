59fe5e626a75468fceae7a02f6f1e036e147eb77
stevefaeembra <stevendkay@gmail.com>
2023-08-09 16:55:48 +0100

    got a simple transaction to work

--- src/script.js
+++ src/script.js
@@ -2,4 +2,5 @@ window.addEventListener("load", function() {
     console.log("Page has finished loading!");
     console.log(window);
     console.log(window.document);
+    gsap.to("body", {backgroundColor: "red", duration: 3});
 });
