deddc69acf8035533945eb81410c9357e1b272a1
stevefaeembra <stevendkay@gmail.com>
2023-08-12 17:30:51 +0100

    got scroll triggers to kick in

--- src/script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", function() {
-    console.log("Page has finished loading!");
-    console.log(window);
-    console.log(window.document);
-    gsap.to("body", {backgroundColor: "red", duration: 3});
-});
